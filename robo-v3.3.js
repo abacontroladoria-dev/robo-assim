9e5ea49d25b79a52f103180feca1b589f3cb2589
Universo ABA <aba.controladoria@gmail.com>
2026-05-04 09:57:53 -0300

    Update robo-v3.3.js

--- robo-v3.3.js
+++ robo-v3.3.js
@@ -662,8 +662,8 @@ async function enviarExcelOrbita(page, arquivoExcel, dataHoje) {
   const dataArquivo = `${dia}-${mes}-${ano}`;
 
   const urlNormal =
-    `https://sirius.assim.com.br/assimcsp/autorizador/preresultado.csp?idHospital=52345&DataIni=30/04/2026&DataFim=30/04/2026&executor=T&natservico=T&servico=T&especialidade=T&amb=&prefeitura=0&tuss=`;
-
+const urlNormal =
+  `https://sirius.assim.com.br/assimcsp/autorizador/preresultado.csp?idHospital=52345&DataIni=${dataHoje}&DataFim=${dataHoje}&executor=T&natservico=T&servico=T&especialidade=T&amb=&prefeitura=0&tuss=`;
   const urlPrefeitura =
     `https://sirius.assim.com.br/assimcsp/autorizador/preresultado.csp?idHospital=52345&DataIni=${dataOntem}&DataFim=${dataOntem}&executor=T&natservico=T&servico=T&especialidade=T&amb=&prefeitura=1&tuss=`;
 
